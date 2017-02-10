7934733ce9b295964d6f57b939a159d8e2fd12b8
todoyu <todoyu@yeah.net>
2017-02-10 10:18:58 +0800

    add install use debian 8

---
+++ add_write_access.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chown www-data:www-data files
+chown www-data:www-data config
+chown www-data:www-data cache 
+chown www-data:www-data log
+chown www-data:www-data install/config
+chown www-data:www-data config/db.php
+chown www-data:www-data config/extensions.php
+chown www-data:www-data config/extconf.php
+chown www-data:www-data config/system.php
+chown www-data:www-data index.html
+
